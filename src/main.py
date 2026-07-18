ff6c1a50a48ed905c80dc3ea5a59b2f851e7fee0
connor bernardo <connorbernardo17@gmail.com>
2026-07-18 17:42:33 -0500

    feat: add semantic search with sentence-transformers

--- src/main.py
+++ src/main.py
@@ -2,25 +2,33 @@ import os
 from dotenv import load_dotenv
 import anthropic
 from pathlib import Path
+from sentence_transformers import SentenceTransformer, util
 
 load_dotenv()
 client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def load_docs(docs_folder: str="docs"):
     """ Read all .md files from the docs folder into memory """
-    path = Path(docs_folder)
+    path = Path(__file__).parent.parent / docs_folder
     result = {}
     for docs in path.glob("*.md"):
         result[docs.name] = docs.read_text()
     return result
 
 def retrieve_relevant_doc(question: str, docs:list):
-    """ Find the doc most relevant to the question """
+    """ Find the doc most relevant to the question using vectors """
     relevant_doc = None
-    for doc in docs:
-        for keyword in question.split():
-            if keyword in docs[doc]:
-                relevant_doc = docs[doc]
+
+    # convert values and question to vectors
+    doc_texts = list(docs.values())
+    doc_vectors = model.encode(doc_texts)
+    question_vector = model.encode(question)
+
+    # rank similiarites and return best score
+    scores = util.cos_sim(question_vector, doc_vectors)
+    best_index = scores.argmax().item()
+    relevant_doc = doc_texts[best_index]
 
     return relevant_doc
 
@@ -42,10 +50,11 @@ def main():
     """ load docs, retrieve, ask, print"""
     docs = load_docs()
     print("I am the MTG rules assistant, please ask me a MTG commander-related question.\n")
-    user_input = input()
-    relevant_doc = retrieve_relevant_doc(user_input, docs)
-    answer = ask_claude(user_input, relevant_doc)
-    print(answer)
+    while True:
+        user_input = input()
+        relevant_doc = retrieve_relevant_doc(user_input, docs)
+        answer = ask_claude(user_input, relevant_doc)
+        print(answer)
 
 if __name__ == "__main__":
     main()
